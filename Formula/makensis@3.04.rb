e1cbb3020472dc75032eacffe9d3cc5a8f259355
idleberg <git@idleberg.com>
2018-12-18 09:09:27 +0100

    add 3.04

---
+++ Formula/makensis@3.04.rb
@@ -0,0 +1,52 @@
+class MakensisAT304 < Formula
+  desc "System to create Windows installers"
+  homepage "https://nsis.sourceforge.io/"
+  url "https://downloads.sourceforge.net/project/nsis/NSIS%203/3.04/nsis-3.04-src.tar.bz2"
+  sha256 "609536046c50f35cfd909dd7df2ab38f2e835d0da3c1048aa0d48c59c5a4f4f5"
+
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "bd5b875da911a5ff1fbdbe1b80ad21e7c2dc5841cd89ba7b9e1d3e412a029bfd" => :mojave
+    sha256 "b656fcbbb32f982ff66c897f8af08b989425f3c375aa96572dde0e00f05cc396" => :high_sierra
+    sha256 "bf01aff6fbcda07ab721b743ca044207face08b9e5f200b764efce8d9adb1c37" => :sierra
+    sha256 "f4516cec938568eb2bea2b162247a10cbd68dedd85c439f5d77170dbc7c5b81b" => :el_capitan
+  end
+
+  option "with-advanced-logging", "Enable advanced logging of all installer actions"
+  option "with-large-strings", "Enable strings up to 8192 characters instead of default 1024"
+  option "with-debug", "Build executables with debugging information"
+
+  depends_on "mingw-w64" => :build
+  depends_on "scons" => :build
+
+  resource "nsis" do
+    url "https://downloads.sourceforge.net/project/nsis/NSIS%203/3.04/nsis-3.04.zip"
+    sha256 "22f3349fea453a45551745635c13e5efb7849ecbdce709daa2b2fa8e2ac55fc4"
+  end
+
+  def install
+    args = [
+      "CC=#{ENV.cc}",
+      "CXX=#{ENV.cxx}",
+      "PREFIX_DOC=#{share}/nsis/Docs",
+      "SKIPUTILS=Makensisw,NSIS Menu,zip2exe",
+      # Don't strip, see https://github.com/Homebrew/homebrew/issues/28718
+      "STRIP=0",
+      "VERSION=#{version}",
+    ]
+
+    args << "NSIS_CONFIG_LOG=yes" if build.with? "advanced-logging"
+    args << "NSIS_MAX_STRLEN=8192" if build.with? "large-strings"
+    args << "DEBUG=1" if build.with? "debug"
+
+    scons "makensis", *args
+    bin.install "build/urelease/makensis/makensis"
+    (share/"nsis").install resource("nsis")
+  end
+
+  test do
+    system "#{bin}/makensis", "-VERSION"
+    system "#{bin}/makensis", "-HDRINFO"
+    system "#{bin}/makensis", "#{share}/nsis/Examples/bigtest.nsi", "-XOutfile /dev/null"
+  end
+end
