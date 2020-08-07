27cd97322b4f927b28ff0294581eb4b477d874e2
idleberg <git@idleberg.com>
2020-08-08 01:04:58 +0200

    add NSIS 3.06

---
+++ Formula/makensis@3.06.1.rb
@@ -0,0 +1,51 @@
+class MakensisAT3061 < Formula
+  desc "System to create Windows installers"
+  homepage "https://nsis.sourceforge.io/"
+  url "https://downloads.sourceforge.net/project/nsis/NSIS%203/3.06.1/nsis-3.06.1-src.tar.bz2"
+  sha256 "9b5d68bf1874a7b393432410c7e8c376f174d2602179883845d2508152153ff0"
+
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "e7cb0cf276e20c96b426188fa69b9a70aff58419747633682be8a957a4c6c166" => :mojave
+    sha256 "c4cd3ba5be94d0c9788997dd9d686b7868519ba2c631e215bdc1eac1ecf63ed0" => :high_sierra
+    sha256 "8f035781e4e926b8dcd367fbdc3a3a2bdd9b5fd96d268da62e9ac88ada495137" => :sierra
+  end
+
+  option "with-advanced-logging", "Enable advanced logging of all installer actions"
+  option "with-large-strings", "Enable strings up to 8192 characters instead of default 1024"
+  option "with-debug", "Build executables with debugging information"
+
+  depends_on "mingw-w64" => :build
+  depends_on "scons" => :build
+
+  resource "nsis" do
+    url "https://downloads.sourceforge.net/project/nsis/NSIS%203/3.06.1/nsis-3.06.1.zip"
+    sha256 "d463ad11aa191ab5ae64edb3a439a4a4a7a3e277fcb138254317254f7111fba7"
+  end
+
+  def install
+    args = [
+      "CC=#{ENV.cc}",
+      "CXX=#{ENV.cxx}",
+      "PREFIX_DOC=#{share}/nsis/Docs",
+      "SKIPUTILS=Makensisw,NSIS Menu,zip2exe",
+      # Don't strip, see https://github.com/Homebrew/homebrew/issues/28718
+      "STRIP=0",
+      "VERSION=#{version}",
+    ]
+
+    args << "NSIS_CONFIG_LOG=yes" if build.with? "advanced-logging"
+    args << "NSIS_MAX_STRLEN=8192" if build.with? "large-strings"
+    args << "DEBUG=1" if build.with? "debug"
+
+    system "scons", "makensis", *args
+    bin.install "build/urelease/makensis/makensis"
+    (share/"nsis").install resource("nsis")
+  end
+
+  test do
+    system "#{bin}/makensis", "-VERSION"
+    system "#{bin}/makensis", "-HDRINFO"
+    system "#{bin}/makensis", "#{share}/nsis/Examples/bigtest.nsi", "-XOutfile /dev/null"
+  end
+end
---
+++ Formula/makensis@3.06.rb
@@ -0,0 +1,51 @@
+class MakensisAT306 < Formula
+  desc "System to create Windows installers"
+  homepage "https://nsis.sourceforge.io/"
+  url "https://downloads.sourceforge.net/project/nsis/NSIS%203/3.06/nsis-3.06-src.tar.bz2"
+  sha256 "be2711fde3373c05d10e619d586139573c45fe1349d1330564ef799bbd2235cc"
+
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "e7cb0cf276e20c96b426188fa69b9a70aff58419747633682be8a957a4c6c166" => :mojave
+    sha256 "c4cd3ba5be94d0c9788997dd9d686b7868519ba2c631e215bdc1eac1ecf63ed0" => :high_sierra
+    sha256 "8f035781e4e926b8dcd367fbdc3a3a2bdd9b5fd96d268da62e9ac88ada495137" => :sierra
+  end
+
+  option "with-advanced-logging", "Enable advanced logging of all installer actions"
+  option "with-large-strings", "Enable strings up to 8192 characters instead of default 1024"
+  option "with-debug", "Build executables with debugging information"
+
+  depends_on "mingw-w64" => :build
+  depends_on "scons" => :build
+
+  resource "nsis" do
+    url "https://downloads.sourceforge.net/project/nsis/NSIS%203/3.06/nsis-3.06.zip"
+    sha256 "948bb8e7ff8804d679d4ea06b13f795c1a0196a1b73cb0944559affc45543050"
+  end
+
+  def install
+    args = [
+      "CC=#{ENV.cc}",
+      "CXX=#{ENV.cxx}",
+      "PREFIX_DOC=#{share}/nsis/Docs",
+      "SKIPUTILS=Makensisw,NSIS Menu,zip2exe",
+      # Don't strip, see https://github.com/Homebrew/homebrew/issues/28718
+      "STRIP=0",
+      "VERSION=#{version}",
+    ]
+
+    args << "NSIS_CONFIG_LOG=yes" if build.with? "advanced-logging"
+    args << "NSIS_MAX_STRLEN=8192" if build.with? "large-strings"
+    args << "DEBUG=1" if build.with? "debug"
+
+    system "scons", "makensis", *args
+    bin.install "build/urelease/makensis/makensis"
+    (share/"nsis").install resource("nsis")
+  end
+
+  test do
+    system "#{bin}/makensis", "-VERSION"
+    system "#{bin}/makensis", "-HDRINFO"
+    system "#{bin}/makensis", "#{share}/nsis/Examples/bigtest.nsi", "-XOutfile /dev/null"
+  end
+end
