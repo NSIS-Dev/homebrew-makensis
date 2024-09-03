ec193b17164fcf74de283fac4a0c1813f778566d
Pierluigi Zagaria <pierluigizagaria@gmail.com>
2024-09-03 18:41:31 +0200

    added scons@4.7.0 formula

---
+++ Formula/scons@4.7.0.rb
@@ -0,0 +1,29 @@
+class SconsAT470 < Formula
+  include Language::Python::Virtualenv
+
+  desc "Substitute for classic 'make' tool with autoconf/automake functionality"
+  homepage "https://www.scons.org/"
+  url "https://files.pythonhosted.org/packages/7b/68/6895065c86c65a9388eb760a43ea695ec5b9b1c98a9675a3bcd682dbe9c0/SCons-4.7.0.tar.gz"
+  sha256 "d8b617f6610a73e46509de70dcf82f76861b79762ff602d546f4e80918ec81f3"
+  license "MIT"
+
+  depends_on "python@3.12"
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      int main()
+      {
+        printf("Homebrew");
+        return 0;
+      }
+    EOS
+    (testpath/"SConstruct").write "Program('test.c')"
+    system bin/"scons"
+    assert_equal "Homebrew", shell_output("#{testpath}/test")
+  end
+end
